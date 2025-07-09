06fb131971ee7f7da3a9350181b940c4becb28a8
pxman1012 <pxman18011997@gmail.com>
2025-07-09 16:40:39 +0700

    sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,6 +1,12 @@
 export default function sitemap() {
   return [
     {
+      url: 'https://pxman-weather-app.vercel.app/du-bao-thoi-tiet',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
       url: 'https://pxman-weather-app.vercel.app',
       lastModified: new Date(),
       changeFrequency: 'yearly',
